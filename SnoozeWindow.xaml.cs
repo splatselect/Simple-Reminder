fca2a90691dd5f53102019f637815f5f3960a04c
idas <idas@guobis.io>
2025-12-03 14:57:10 -0600

    Snooze button timers extended

--- SnoozeWindow.xaml.cs
+++ SnoozeWindow.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Windows;
+using System.Windows.Input;
 
 namespace ReminderApp
 {
@@ -19,13 +20,66 @@ namespace ReminderApp
             UpdateTimeDisplay();
         }
 
+        private void MinutesSlider_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            // If slider is at or near maximum when released, extend it by 5 hours (300 minutes)
+            if (MinutesSlider.Value >= MinutesSlider.Maximum - 1)
+            {
+                MinutesSlider.Maximum += 300; // Add 5 more hours
+            }
+        }
+
         private void UpdateTimeDisplay()
         {
-            if (TimeDisplay == null)
+            if (TimeDisplay == null || ReminderTimeDisplay == null)
                 return;
 
             int minutes = (int)MinutesSlider.Value;
-            TimeDisplay.Text = $"{minutes} minute{(minutes != 1 ? "s" : "")}";
+
+            // Format display text
+            if (minutes < 60)
+            {
+                TimeDisplay.Text = $"{minutes} minute{(minutes != 1 ? "s" : "")}";
+            }
+            else if (minutes < 1440) // Less than 24 hours
+            {
+                int hours = minutes / 60;
+                int remainingMinutes = minutes % 60;
+                if (remainingMinutes == 0)
+                {
+                    TimeDisplay.Text = $"{hours} hour{(hours != 1 ? "s" : "")}";
+                }
+                else
+                {
+                    TimeDisplay.Text = $"{hours}h {remainingMinutes}m";
+                }
+            }
+            else // 24 hours or more
+            {
+                int days = minutes / 1440;
+                int remainingHours = (minutes % 1440) / 60;
+                int remainingMinutes = minutes % 60;
+
+                if (remainingHours == 0 && remainingMinutes == 0)
+                {
+                    TimeDisplay.Text = $"{days} day{(days != 1 ? "s" : "")}";
+                }
+                else if (remainingMinutes == 0)
+                {
+                    TimeDisplay.Text = $"{days}d {remainingHours}h";
+                }
+                else
+                {
+                    TimeDisplay.Text = $"{days}d {remainingHours}h {remainingMinutes}m";
+                }
+            }
+
+            // Show when the snoozed reminder will appear
+            var dueTime = DateTime.Now.AddMinutes(minutes);
+            var timeString = dueTime.Date == DateTime.Now.Date
+                ? $"Today at {dueTime:h:mm tt}"
+                : $"{dueTime:MMM d} at {dueTime:h:mm tt}";
+            ReminderTimeDisplay.Text = $"Snooze until: {timeString}";
         }
 
         private void Snooze_Click(object sender, RoutedEventArgs e)
